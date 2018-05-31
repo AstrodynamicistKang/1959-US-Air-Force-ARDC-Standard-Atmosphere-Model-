900089e7ad7e84ec86a4e23d508e618a4b355915
Nicholas Kang <38155546+AstrodynamicistKang@users.noreply.github.com>
2018-05-31 17:30:57 +0800

    First stable release
    temp(), pres(), dens(), met2pot(), pot2met()

--- 1959 USAF ARDC Standard Atmosphere Model.py
+++ 1959 USAF ARDC Standard Atmosphere Model.py
@@ -1,4 +1,6 @@
-print("This is a Python program that computes atmospheric parameters such as air density, pressure and temperature based on the 1959 USAF ARDC Stanard Atmosphere Model.")
+print('''This is a Python program that computes atmospheric parameters such as
+air density, pressure and temperature based on the 1959 USAF ARDC Standard
+Atmosphere Model.\n''')
 print('''
     Copyright (C) 2018 Nicholas Kang
     
@@ -13,12 +15,20 @@ print('''
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.\n''')
-print("Feel free to contact the author at zt.kang@gmail.com or PM 'Nicholas Kang' in Orbiter Forum.")
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.\n\n''')
+print("Feel free to contact the author at zt.kang@gmail.com or PM 'Nicholas Kang' in Orbiter Forum for bug reports and/or comments.")
+
+from math import exp
 
 geopotential_altitude = [-5000, 0, 11000, 25000, 47000, 53000, 79000, 90000, 105000, 160000, 170000, 200000, 700000]
 temperature = [320.66, 288.16, 216.66, 216.66, 282.66, 282.66, 165.66, 165.66, 225.66, 1325.66, 1425.66, 1575.66, 3325.66]
 lapse_rate = [-0.0065, -0.0065, 0, 0.0030, 0, -0.0045, 0, 0.0040, 0.02, 0.01, 0.0050, 0.0035]
+
+g_0 = 9.80665
+R = 287
+r = 6356766
+p_0 = 101325
+rho_0 = 1.2250140
 
 if len(geopotential_altitude) == len(temperature) == (len(lapse_rate) + 1):
     pass
@@ -27,24 +37,87 @@ else:
     print('geopot_altitude_list_mag = {}, temperature_list_mag = {}, lapse_rate_list_mag = {}'.format(len(geopotential_altitude), len(temperature), len(lapse_rate)))
     print("Length of lists not the same. Database error.")
 
+def met2pot(Z):
+    'Convert geometric altitude to geopotential altitude.'
+    H = (r*Z)/(r + Z)
+    return H
+
+def pot2met(H):
+    'Convert geopotential altitude to geometric altitude.'
+    Z = (r*H)/(r - H)
+    return Z
+
 def temp(x):
-    'Calculate temperature of air given the geopotential altitude of the atmosphere in range -5000 m to 700000 m (-5 km to 700 km) ONLY'
+    'Calculate the air temperature at a given geopotential altitude'
     if x < -5000 or x > 700000:
         print("Input value out of range. Min accepted value = -5000; Max accepted value = 700000.")
     else:
         for i in geopotential_altitude:
             if x == i:
-                print("The temperature of the atmosphere at a geopotential altitude of {} m is {} degree Kelvin.".format(x, temperature[geopotential_altitude.index(i)]))
-                break
+                return temperature[geopotential_altitude.index(i)]
             elif x < geopotential_altitude[geopotential_altitude.index(i) + 1]:
                 if lapse_rate[geopotential_altitude.index(i)] == 0:
-                    print("The temperature of the atmosphere at a geopotential altitude of {} m is {} degree Kelvin.".format(x, temperature[geopotential_altitude.index(i)]))
+                    return temperature[geopotential_altitude.index(i)]
                 else:
                     T = (lapse_rate[geopotential_altitude.index(i)])*(x - i) + temperature[geopotential_altitude.index(i)]
-                    print("The temperature of the atmosphere at a geopotential altitude of {} m is {} degree Kelvin.".format(x, T))
+                    return T
                 break
 
-
-                
-            
+def pres(x):
+    'Calculate the atmospheric pressure at a given geopotential altitude.'
+    if x < -5000 or x > 700000:
+        print("Input value out of range. Min accepted value = -5000; Max accepted value = 700000.")
+    else:
+        if x < 0:
+            p = p_0*((288.16/(temp(x)))**(g_0/(R*-0.0065)))
+            return p
+        else:
+            p_temp = p_0
+            for i in geopotential_altitude[1:]:
+                if x >= i:
+                    if x <= geopotential_altitude[geopotential_altitude.index(i) + 1]:
+                        if lapse_rate[geopotential_altitude.index(i)] != 0:
+                            p_temp = p_temp*((temp(i)/(temp(i) + (lapse_rate[geopotential_altitude.index(i)])*(x - i)))**(g_0/(R*lapse_rate[geopotential_altitude.index(i)])))
+                            print(p_temp, 'a', sep='')
+                        elif lapse_rate[geopotential_altitude.index(i)] == 0:
+                            p_temp = p_temp*exp((-g_0*(x - i))/(R*temp(i)))
+                            print(p_temp, 'b', sep='')
+                    elif x > geopotential_altitude[geopotential_altitude.index(i) + 1]:
+                        a = geopotential_altitude[geopotential_altitude.index(i) + 1]
+                        if lapse_rate[geopotential_altitude.index(i)] != 0:
+                            p_temp = p_temp*((temp(i)/(temp(i) + (lapse_rate[geopotential_altitude.index(i)])*(a - i)))**(g_0/(R*lapse_rate[geopotential_altitude.index(i)])))
+                            print(p_temp, 'c', sep='')
+                        elif lapse_rate[geopotential_altitude.index(i)] == 0:
+                            p_temp = p_temp*exp((-g_0*(a - i))/(R*temp(i)))
+                            print(p_temp, 'd', sep='')
+            return p_temp
+                             
+def dens(x):
+    'Calculate the air density at a given geopotential altitude.'
+    if x < -5000 or x > 700000:
+        print("Input value out of range. Min accepted value = -5000; Max accepted value = 700000.")
+    else:
+        if x < 0:
+            rho = rho_0*((288.16/(temp(x)))**((g_0/(R*-0.0065) + 1)))
+            return rho
+        else:
+            rho_temp = rho_0
+            for i in geopotential_altitude[1:]:
+                if x >= i:
+                    if x <= geopotential_altitude[geopotential_altitude.index(i) + 1]:
+                        if lapse_rate[geopotential_altitude.index(i)] != 0:
+                            rho_temp = rho_temp*((temp(i)/(temp(i) + (lapse_rate[geopotential_altitude.index(i)])*(x - i)))**((g_0/(R*lapse_rate[geopotential_altitude.index(i)]) + 1)))
+                            print(rho_temp, 'a', sep='')
+                        elif lapse_rate[geopotential_altitude.index(i)] == 0:
+                            rho_temp = rho_temp*exp((-g_0*(x - i))/(R*temp(i)))
+                            print(rho_temp, 'b', sep='')
+                    elif x > geopotential_altitude[geopotential_altitude.index(i) + 1]:
+                        a = geopotential_altitude[geopotential_altitude.index(i) + 1]
+                        if lapse_rate[geopotential_altitude.index(i)] != 0:
+                            rho_temp = rho_temp*((temp(i)/(temp(i) + (lapse_rate[geopotential_altitude.index(i)])*(a - i)))**((g_0/(R*lapse_rate[geopotential_altitude.index(i)]) + 1)))
+                            print(rho_temp, 'c', sep='')
+                        elif lapse_rate[geopotential_altitude.index(i)] == 0:
+                            rho_temp = rho_temp*exp((-g_0*(a - i))/(R*temp(i)))
+                            print(rho_temp, 'd', sep='')
+            return rho_temp
 
